516b4b70e37dc0e767a199029b62d65f73bb4d7c
Mauro J. Sanchirico III <sanchirico.mauro@gmail.com>
2024-09-28 11:27:52 -0400

    ""

--- scripts/nbia_download.tcl
+++ scripts/nbia_download.tcl
@@ -9,18 +9,34 @@
 # The Cancer Imaging Archive (TCIA). This script is intended for use inside the
 # devcontainer that accompanies this repository.
 #
+# Arguments:
+#    Argument 1 - specify the type of download: valid values are DOWNLOAD (use
+#    for a new download), REDOWNLAD (for redownloading a previous download),
+#    and RESUME (for resuming an unfinished download).
+#
+#    Argument 2 - specify the timeout: specify the maximum time to wait for
+#    the download to complete in seconds (e.g., 30). Use -1 for no timeout.
+#    No timeout is the preferred setting for long downloads.
+#
+#    Argument 3 - specify the manifest file: This is the full unix path to
+#    the manifest file, e.g., /path/to/manifest.tcia.
+#
+#    Argument 4 - specify the location to store the download files: This is the
+#    full path to the download location, e.g., /path/to/downloads.
+#
 # Dependencies:
+#
 # This script assumes that expect is already installed in the environment.
 #
 # Usage:
 #
-# expect nbia_download.tcl (DOWNLOAD | REDOWNLOAD | RESUME) <manifest_filename> <download_location>
+# expect nbia_download.tcl (DOWNLOAD | REDOWNLOAD | RESUME) <timeout> <manifest_filename> <download_location>
 #
 # Examples:
 #
 # Use the DOWNLOAD argument to start a new download.
 #
-# expect nbia_download.tcl DOWNLOAD \
+# expect nbia_download.tcl DOWNLOAD -1 \
 #    /workspaces/data-loaders/nbia_manifest_files/small_test_manifest/manifest-*.tcia \
 #    /workspaces/data-loaders/nbia_downloads/
 #
@@ -29,14 +45,14 @@
 # redownload all files in the manifest, even if some files are already present
 # in the specified downloads folder.
 #
-# expect nbia_download.tcl REDOWNLOAD \
+# expect nbia_download.tcl REDOWNLOAD -1 \
 #    /workspaces/data-loaders/nbia_manifest_files/small_test_manifest/manifest-*.tcia \
 #    /workspaces/data-loaders/nbia_downloads/
 #
 # Use the RESUME argument to resume a previous download. This will only download
 # files not present in the previous download.
 #
-# expect nbia_download.tcl RESUME \
+# expect nbia_download.tcl RESUME -1 \
 #    /workspaces/data-loaders/nbia_manifest_files/small_test_manifest/manifest-*.tcia \
 #    /workspaces/data-loaders/nbia_downloads/
 #
@@ -48,7 +64,7 @@ set VERSION "0.0.1"
 set DOWNLOAD_ARG "DOWNLOAD"
 set REDOWNLOAD_ARG "REDOWNLOAD"
 set RESUME_ARG "RESUME"
-set CORRECT_ARGC 3
+set CORRECT_ARGC 4
 
 # Documentation string for usage explanations
 set DOCUMENTATION {
@@ -61,11 +77,26 @@ Dependencies:
 
 This script assumes that expect is already installed in the environment.
 
+Arguments:
+   Argument 1 - specify the type of download: valid values are DOWNLOAD (use
+   for a new download), REDOWNLAD (for redownloading a previous download),
+   and RESUME (for resuming an unfinished download).
+
+   Argument 2 - specify the timeout: specify the maximum time to wait for
+   the download to complete in seconds (e.g., 30). Use -1 for no timeout.
+   No timeout is the preferred setting for long downloads.
+
+   Argument 3 - specify the manifest file: This is the full unix path to
+   the manifest file, e.g., /path/to/manifest.tcia.
+
+   Argument 4 - specify the location to store the download files: This is the
+   full path to the download location, e.g., /path/to/downloads.
+
 Examples:
 
 Use the DOWNLOAD argument to start a new download.
 
-expect nbia_download.tcl DOWNLOAD\
+expect nbia_download.tcl DOWNLOAD -1\
     /workspaces/data-loaders/nbia_manifest_files/small_test_manifest/manifest-*.tcia\
     /workspaces/data-loaders/nbia_downloads/
 
@@ -74,14 +105,14 @@ dataset (e.g., if the previously downloaded dataset is corrupted). This will
 redownload all files in the manifest, even if some files are already present
 in the specified downloads folder.
 
-expect nbia_download.tcl REDOWNLOAD\
+expect nbia_download.tcl REDOWNLOAD -1\
 /workspaces/data-loaders/nbia_manifest_files/small_test_manifest/manifest-*.tcia\
 /workspaces/data-loaders/nbia_downloads/
 
 Use the RESUME argument to resume a previous download. This will only download
 files not present in the previous download.
 
-expect nbia_download.tcl RESUME\
+expect nbia_download.tcl RESUME -1\
 /workspaces/data-loaders/nbia_manifest_files/small_test_manifest/manifest-*.tcia\
 /workspaces/data-loaders/nbia_downloads/
 }
@@ -104,10 +135,11 @@ proc print_usage {} {
     set usage_string ""
     append usage_string "Usage: expect $SCRIPT_NAME"
     append usage_string " ($DOWNLOAD_ARG | $REDOWNLOAD_ARG | $RESUME_ARG)"
+    append usage_string " (-1 | <timeout value>)"
     append usage_string " <manifest_filename> <download_location>"
 
     puts "$usage_string"
-    puts "Example: expect scripts/nbia_download.tcl DOWNLOAD /workspaces/data-loaders/nbia_manifest_files/small_test_manifest/manifest-*.tcia /workspaces/data-loaders/nbia_downloads/"
+    puts "Example: expect scripts/nbia_download.tcl DOWNLOAD -1 /workspaces/data-loaders/nbia_manifest_files/small_test_manifest/manifest-*.tcia /workspaces/data-loaders/nbia_downloads/"
     puts "Help: expect scripts/nbia_download.tcl --help"
     puts "Version: expect scripts/nbia_download.tcl --version"
 }
@@ -164,10 +196,13 @@ proc check_for_options {argv} {
         if {[string match "--help" $arg]} {
             print_help
             exit 0
-        }
-        if {[string match "--version" $arg]} {
+        } elseif {[string match "--version" $arg]} {
             print_version
             exit 0
+        } else {
+            puts "Invalid input argument."
+            print_usage
+            exit -1
         }
     }
 }
@@ -191,11 +226,13 @@ proc check_argument_count {argc} {
 }
 
 proc extract_valid_input_arguments {argv argc} {
-    check_for_options $argv
+    if { $argc == 1 } {
+        check_for_options $argv
+    }
     check_argument_count $argc
     set download_mode [lindex $argv 0]
-    set manifest_file [lindex $argv 1]
-    set download_location [lindex $argv 2]
+    set manifest_file [lindex $argv 2]
+    set download_location [lindex $argv 3]
     set download_mode [string toupper $download_mode]
     return [list $download_mode $manifest_file $download_location]
 }
@@ -204,32 +241,48 @@ set validated_arguments [extract_valid_input_arguments $argv $argc]
 set download_mode [lindex $validated_arguments 0]
 set manifest_file [lindex $validated_arguments 1]
 set download_location [lindex $validated_arguments 2]
-set timeout 10
-spawn /opt/nbia-data-retriever/nbia-data-retriever --cli "$manifest_file" -d "$download_location" -v -F
+
+set timeout [lindex $argv 1]
+
+spawn /opt/nbia-data-retriever/nbia-data-retriever \
+    --cli "$manifest_file" \
+    -d "$download_location" \
+    -v -F
+
 expect "Do you agree with the Data Usage Agreement? (Y/N)"
 send "Y\n"
 switch $download_mode {
     "DOWNLOAD" {
         # Since this is a first download, there are no other interactions to
         # be expected.
-        expect eof
+        puts " (nbia_download): Downloading new files..."
     }
     "REDOWNLOAD" {
         # Since this is a complete redownload, we need all the files.
+        puts " (nbia_download): Redownloading files..."
         expect "Enter A for downloading all, M for downloading missing series. E for exiting the program:"
         send "A\n"
-        expect eof
     }
     "RESUME" {
         # Since this is a resumed download, we only need to download the
         # missing series.
+        puts " (nbia_download): Resuming previous download..."
         expect "Enter A for downloading all, M for downloading missing series. E for exiting the program:"
         send "M\n"
-        expect eof
     }
     default {
         puts "Unknown argument to $SCRIPT_NAME: $download_mode"
         print_usage
         exit 1
+    }
+}
+
+puts " (nbia_download): Complete!"
+
+expect {
+    eof {
+        puts " (nbia_download): Reached end of file. Exiting normally."
+    } timeout {
+        puts "Connection timed out. Download was not successful."
     }
 }
